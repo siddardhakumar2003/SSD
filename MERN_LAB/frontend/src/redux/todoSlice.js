fd83df56c93abe14bf4dda2209f38d077cdde9c7
siddardhakumar2003 <158472582+siddardhakumar2003@users.noreply.github.com>
2025-10-25 22:41:33 +0530

    Add files via upload

---
+++ MERN_LAB/frontend/src/redux/todoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    setTodos: (state, action) => {
+      state.todos = action.payload;
+    },
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter(todo => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { setTodos, addTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
